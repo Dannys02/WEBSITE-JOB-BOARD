8f055beb33b1c1ba9d9f20554f2c00822ce621bb
Dannys02 <webdannys@gmail.com>
2026-03-16 10:01:27 +0700

    buat file controller

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+  public function register(Request $request) {
+    $request->validate([
+      'name' => 'required|string|max:255',
+      'email' => 'required|email|unique:users',
+      'password' => 'required|min:8|confirmed',
+      'role' => 'required|in:seeker,employer',
+    ]);
+
+    $user = User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+      'role' => $request->role,
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+      'message' => 'Register berhasil',
+      'user' => $user,
+      'token' => $token,
+    ], 201);
+  }
+}
